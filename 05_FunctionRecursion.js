c2f61dcaf6f0d57a5a06e24218ee7287d4805e5c
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-16 23:44:00 +0530

    recursion

--- 05_FunctionRecursion.js
+++ 05_FunctionRecursion.js
@@ -9,3 +9,16 @@ function loop(val) {
   loop(temp);
 }
 loop(counter);
+
+//or
+
+function loop2(val) {
+  console.log(val);
+  if (val > 0) {
+    val--;
+    return loop2(val);
+  }
+  return "end";
+}
+const looper = loop2(25);
+console.log(looper);
