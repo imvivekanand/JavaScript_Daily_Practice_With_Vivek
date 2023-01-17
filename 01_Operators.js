76e450703b257c068e0a601691a7e4a8e98ac758
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-18 01:19:09 +0530

    TO

---
+++ 01_Operators.js
@@ -0,0 +1,17 @@
+//Assignment Operator 
+
+//Comparision operator
+
+1 == "1"
+2 === 2
+1 <= 2
+1 != 2
+
+//Logical operator 
+
+// && -> And Operator 
+// || -> Or Operator 
+// ! -> Not Operator
+
+
+//Ternary Operator
