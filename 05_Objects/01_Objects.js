5530a10b6e5f9c285e4e61a14e7ea3c1d05dc95d
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-22 00:13:27 +0530

    OBJ

--- 05_Objects/01_Objects.js
+++ 05_Objects/01_Objects.js
@@ -8,7 +8,7 @@ const obj = {
   V4: 'N',
   V5: 'D',
 };
-
+obj.newkey = "VIVEK"
 console.log(obj);
 console.log(obj.first + " " + obj.last);
 console.log(obj['first'] + " " + obj['last']);
@@ -17,3 +17,15 @@ for(let x=1;x<=5;x++){
     console.log("V"+x);
     console.log(obj["V"+x]);
 }
+
+const obj1 = obj;
+obj1.newkey = "Anand";
+console.log(obj1);
+console.log(obj);
+
+
+//We cannot use . notation where the key name has spaces.
+//In that case we have to use the [].
+
+//jsonlint.com - Allow us to make the json data into readable format.
+//Js Objects can have fuctions inside them whereas JSON do not.
