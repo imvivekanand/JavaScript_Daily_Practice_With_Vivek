fce5d0216f68b17b695944115379c20051900769
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-24 02:10:12 +0530

    shift unshift

--- 06_Array/02_JavaScriptUpdateArray.js
+++ 06_Array/02_JavaScriptUpdateArray.js
@@ -6,9 +6,17 @@ for (let x = 0; x < 7; x++) {
   billa.push(pet);
 }
 for (let x = 0; x<2; x++) {
-  //   cat[billa.length] = "billa" + x;
   const angrypet = billa.pop();
   console.log('run away: ' + angrypet);
+}
+for (let x = 0; x<2; x++) {
+  const angrypet = billa.shift();
+  console.log('run away earlier: ' + angrypet);
+}
+for (let x = 0; x<2; x++) {
+  const happypet = 'Add new billa ' + x;
+  billa.unshift(happypet);
+  console.log('Came home: ' + happypet);
 }
 console.log(billa);
 console.log(cat);
