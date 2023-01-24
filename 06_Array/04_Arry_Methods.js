0aea42d91178e67617b48e0d3f8fad9e1b21faf3
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-25 01:04:16 +0530

    Array Methods

---
+++ 06_Array/04_Arry_Methods.js
@@ -0,0 +1,32 @@
+let mixtureArray = [
+  "one",
+  "two",
+  "ka",
+  "four",
+  08,
+  12,
+  96,
+  true,
+  {
+    Amit: "Gaurav",
+    Sumit: "Saurav",
+    Rohit: "Srivastava",
+    Ritik: "Roshan",
+    Wadle: "Don",
+  },
+];
+
+let val = mixtureArray.toString();
+val = JSON.stringify(mixtureArray);
+val = JSON.parse(val);
+
+val[0] = "badal do is element ko";
+
+console.log(val);
+console.log(mixtureArray);
+
+let stringWalaArray = ["Vivek", "Anand", "WEB3", "JavaScript", "React"];
+
+val = stringWalaArray.join("{|Ye Hai Mera wala Saperator|/}");
+val = stringWalaArray.concat(mixtureArray);
+console.log(val);
