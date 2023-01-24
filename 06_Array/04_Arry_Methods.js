6f86115c866061556c1e86d7b2cafff9d409e2fa
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-25 01:31:52 +0530

    Include_Method

--- 06_Array/04_Arry_Methods.js
+++ 06_Array/04_Arry_Methods.js
@@ -29,4 +29,8 @@ let stringWalaArray = ["Vivek", "Anand", "WEB3", "JavaScript", "React"];
 
 val = stringWalaArray.join("{|Ye Hai Mera wala Saperator|/}");
 val = stringWalaArray.concat(mixtureArray);
+val=stringWalaArray.includes("React")
+val=stringWalaArray.indexOf("React")
+val=Array.isArray(stringWalaArray);
+
 console.log(val);
