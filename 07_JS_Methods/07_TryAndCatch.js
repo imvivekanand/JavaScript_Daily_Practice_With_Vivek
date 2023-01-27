5a841565835ebde478fd197e28b8599c895ef55a
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-27 22:44:04 +0530

    Try And Catch

---
+++ 07_JS_Methods/07_TryAndCatch.js
@@ -0,0 +1,19 @@
+//throw is used to throw a custom error.
+
+function tester(val){
+    try{
+        if (val==undefined) throw "no value";
+        if (isNaN(val)) throw "not a number";
+        val = Number(val);
+    }
+    catch(err){
+        console.log(`There was an error ${err} = ${val}`);
+    }
+    finally{
+        console.log(`Done ${val}`);
+    }
+}
+
+tester();
+tester(100);
+tester('Hello');
