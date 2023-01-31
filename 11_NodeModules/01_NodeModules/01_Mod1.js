78484c44763d466659a168bf4b5b7ece91d2d818
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-31 10:41:39 +0530

    Node Module Intro

---
+++ 11_NodeModules/01_NodeModules/01_Mod1.js
@@ -0,0 +1,12 @@
+function test1(){
+    console.log('test1');
+}
+
+test1();
+
+//const test1 = 'hello';
+
+// console.log(module);
+console.log(module.filename);
+console.log(module.loaded);
+console.log(module.path);
