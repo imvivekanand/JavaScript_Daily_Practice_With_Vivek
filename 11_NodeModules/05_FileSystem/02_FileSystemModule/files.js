a6c609784bfa3eca1791929da532561361234ec0
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-02-06 00:07:40 +0530

    Filejs comp

--- 11_NodeModules/05_FileSystem/02_FileSystemModule/files.js
+++ 11_NodeModules/05_FileSystem/02_FileSystemModule/files.js
@@ -6,15 +6,22 @@ const dir = './public';
 let tempCounter = 1;
 let newFileName = `new${tempCounter}.html`;
 
+function maker(){
+for (let i=0; i<5; i++){
 fs.readdir(dir, (err, files)=>{
     console.log(files);
     console.log(files.length);
     console.log(err);
-    tempCounter = files.length + 1;
+    tempCounter = files.length + i + 1;
     newFileName = `new${tempCounter}.html`
     createNewFile(newFileName);
 })
+}}
 
-function createNewFile(fileName){
-    
+function createNewFile(myfileName){
+    const html = `<h1>Hello ${tempCounter}</h1>`;
+    fs.appendFile(dir + '/' + myfileName, html, (err)=>{
+        if(err) throw err;
+        console.log(`Save File ${myfileName}`)
+    })
 }
