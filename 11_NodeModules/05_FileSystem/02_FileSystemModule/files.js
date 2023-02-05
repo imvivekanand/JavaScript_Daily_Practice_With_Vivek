2adfff177ab484aad545418b8371c4ad22c5a678
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-02-05 23:46:37 +0530

    filejs

--- 11_NodeModules/05_FileSystem/02_FileSystemModule/files.js
+++ 11_NodeModules/05_FileSystem/02_FileSystemModule/files.js
@@ -1,2 +1,20 @@
+const { log } = require('console');
 const fs = require('fs');
-console.log(fs);
+// console.log(fs);
+
+const dir = './public';
+let tempCounter = 1;
+let newFileName = `new${tempCounter}.html`;
+
+fs.readdir(dir, (err, files)=>{
+    console.log(files);
+    console.log(files.length);
+    console.log(err);
+    tempCounter = files.length + 1;
+    newFileName = `new${tempCounter}.html`
+    createNewFile(newFileName);
+})
+
+function createNewFile(fileName){
+    
+}
