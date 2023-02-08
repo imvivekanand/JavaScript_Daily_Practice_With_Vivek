f2ab92ce513eca9dd43ef9396577712b78c92f33
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-02-08 23:35:55 +0530

    Update Files

--- 11_NodeModules/05_FileSystem/02_FileSystemModule/files.js
+++ 11_NodeModules/05_FileSystem/02_FileSystemModule/files.js
@@ -18,6 +18,24 @@ fs.readdir(dir, (err, files)=>{
 })
 }}
 
+myLog('test2');
+function overmyLog(val){ //overwriting file /create
+    const html = ` -${val}`;
+    fs.writeFile(dir + `/log.html`, html, (err)=>{
+        if(err) throw err;
+        console.log(`Updated writeFile ${val}`);
+    })
+}
+
+function myLog(val){ //adding / create
+    const html = `<br> * ${val}`;
+    fs.appendFile(dir + `/log.html`, html, (err)=>{
+        if(err) throw err;
+        console.log(`*Appended  ${val}`);
+    })
+}
+
+
 function createNewFile(myfileName){
     const html = `<h1>Hello ${tempCounter}</h1>`;
     fs.appendFile(dir + '/' + myfileName, html, (err)=>{
