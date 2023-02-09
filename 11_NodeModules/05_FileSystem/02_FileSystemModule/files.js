731bf5bea72015e7c8242ae10ba76d43ceaf188b
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-02-09 23:46:40 +0530

    Del file

--- 11_NodeModules/05_FileSystem/02_FileSystemModule/files.js
+++ 11_NodeModules/05_FileSystem/02_FileSystemModule/files.js
@@ -1,45 +1,65 @@
-const { log } = require('console');
-const fs = require('fs');
+const { log } = require("console");
+const fs = require("fs");
 // console.log(fs);
 
-const dir = './public';
+const dir = "./public";
 let tempCounter = 1;
 let newFileName = `new${tempCounter}.html`;
 
-function maker(){
-for (let i=0; i<5; i++){
-fs.readdir(dir, (err, files)=>{
-    console.log(files);
-    console.log(files.length);
-    console.log(err);
-    tempCounter = files.length + i + 1;
-    newFileName = `new${tempCounter}.html`
-    createNewFile(newFileName);
-})
-}}
+function maker() {
+  for (let i = 0; i < 5; i++) {
+    fs.readdir(dir, (err, files) => {
+      console.log(files);
+      console.log(files.length);
+      console.log(err);
+      tempCounter = files.length + i + 1;
+      newFileName = `new${tempCounter}.html`;
+      createNewFile(newFileName);
+    });
+  }
+}
 
-myLog('test2');
-function overmyLog(val){ //overwriting file /create
-    const html = ` -${val}`;
-    fs.writeFile(dir + `/log.html`, html, (err)=>{
-        if(err) throw err;
-        console.log(`Updated writeFile ${val}`);
+removeAll()
+function removeAll(){
+    fs.readdir(dir, (err, files)=>{
+        files.forEach((myFile)=>{
+            delFile(dir + '/' + myFile);
+        })
     })
 }
 
-function myLog(val){ //adding / create
-    const html = `<br> * ${val}`;
-    fs.appendFile(dir + `/log.html`, html, (err)=>{
-        if(err) throw err;
-        console.log(`*Appended  ${val}`);
-    })
+// delFile(dir + "/log.html");
+function delFile(val) {
+  fs.unlink(val, (err) => {
+    if (err) throw err;
+    console.log(`file Deleted ${val}`);
+  });
 }
 
+// myLog('test2');
 
-function createNewFile(myfileName){
-    const html = `<h1>Hello ${tempCounter}</h1>`;
-    fs.appendFile(dir + '/' + myfileName, html, (err)=>{
-        if(err) throw err;
-        console.log(`Save File ${myfileName}`)
-    })
+function overmyLog(val) {
+  //overwriting file /create
+  const html = ` -${val}`;
+  fs.writeFile(dir + `/log.html`, html, (err) => {
+    if (err) throw err;
+    console.log(`Updated writeFile ${val}`);
+  });
+}
+
+function myLog(val) {
+  //adding / create
+  const html = `<br> * ${val}`;
+  fs.appendFile(dir + `/log.html`, html, (err) => {
+    if (err) throw err;
+    console.log(`*Appended  ${val}`);
+  });
+}
+
+function createNewFile(myfileName) {
+  const html = `<h1>Hello ${tempCounter}</h1>`;
+  fs.appendFile(dir + "/" + myfileName, html, (err) => {
+    if (err) throw err;
+    console.log(`Save File ${myfileName}`);
+  });
 }
