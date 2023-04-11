f5a9fe49623bb7b8351a676f6e6315df4f794037
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-04-11 23:04:41 +0530

    p1 done

--- 14_JavaScriptProjects/01_Project/main.js
+++ 14_JavaScriptProjects/01_Project/main.js
@@ -1,1 +1,6 @@
-console.log("Vivek Anand");
+// let firstName = prompt("Enter your first name?");
+// let lastName = prompt("Enter your last name?");
+// let age = parseInt(prompt("Enter your age?"));
+// age+=2;
+
+// console.log(firstName + " " + lastName + ". " + "Your age is " + age + " years.")
