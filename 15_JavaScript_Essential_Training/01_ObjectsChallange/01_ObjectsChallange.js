8eb61502e752006fe9dc188ff1b37d421775356a
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-07-25 01:26:06 +0530

    learning from linkedin

---
+++ 15_JavaScript_Essential_Training/01_ObjectsChallange/01_ObjectsChallange.js
@@ -0,0 +1,12 @@
+const iphone = {
+    model:12,
+    size: "6.1 inch",
+    camera: "12mp",
+    "battery capacity": "2600 mah",
+    audio: "dolby",
+    accessories: {
+        audio: "Airpods",
+        charging: "adapter",
+        camera: "lens guard"
+    }
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+let p = 10000;
+let r = 10;
+let t = 2;
+
+let si = (p*r*t)/100;
+
+console.log(si);
+console.log(p);
+console.log(r);
+console.log(t);
