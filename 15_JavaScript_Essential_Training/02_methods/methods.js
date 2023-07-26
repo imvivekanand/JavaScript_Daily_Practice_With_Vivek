d648459429765e51c39edc19b5dbc5a819e12287
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-07-26 11:25:44 +0530

    methods

---
+++ 15_JavaScript_Essential_Training/02_methods/methods.js
@@ -0,0 +1,12 @@
+const vivek = {
+    height: 167,
+    newHeight: function(length){
+        this.height = length;
+    }
+}
+
+console.log(vivek.height)
+
+vivek.newHeight(171);
+
+console.log(vivek.height);
