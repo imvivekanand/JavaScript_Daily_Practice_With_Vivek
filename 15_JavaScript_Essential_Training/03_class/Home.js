09dbd83aeeda9e397a97a4ec5886cb799d68b342
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-07-26 21:28:46 +0530

    class

---
+++ 15_JavaScript_Essential_Training/03_class/Home.js
@@ -0,0 +1,9 @@
+class Home {
+  constructor(rooms, kitchen, hall) {
+    this.rooms = rooms;
+    this.kitchen = kitchen;
+    this.hall = hall;
+  }
+}
+
+export default Home;
---
+++ 15_JavaScript_Essential_Training/03_class/home1.js
@@ -0,0 +1,5 @@
+import Home from "./Home.js";
+
+const homeOne = new Home(3, 1, 1);
+
+console.log(homeOne.kitchen());
